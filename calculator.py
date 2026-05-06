936faa8230092b4000b3f033e9dcf283525bf437
Praghna LK <praghna162@gmail.com>
2026-05-06 18:48:57 +0530

    Add files via upload
    Added beginner Python programs

---
+++ calculator.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter number 1:"))
+num2=int(input("Enter number 2:"))
+print("Addition=",num1+num2)
+print("Subtraction=",num1-num2)
+print("Multiplication=",num1*num2)
+print("Division:=",num1/num2)
+print("Floor Division=",num1//num2)
+print("Power=",num1**num2)
---
+++ table.py
@@ -0,0 +1,4 @@
+num=int(input("Enter a number:"))
+print("The Multiplication Table")
+for i in range(1,13):
+    print(num,"x",i,"=",num*i)
